4e3b9bc143997ca1e5c409ea5fc94f69b441ce44
AndRoniN <andrewronin@mail.ru>
2019-08-05 10:56:48 +0300

    ssh key update

--- Jenkins/topic1/Kubernetes/Kubectl/script.sh
+++ Jenkins/topic1/Kubernetes/Kubectl/script.sh
@@ -6,6 +6,6 @@ kubectl apply -f jenkins-deployment.yml --namespace=jenkins
 kubectl apply -f jenkins-service.yml --namespace=jenkins
 kubectl apply -f jenkins-ingress.yml --namespace=jenkins
 
-kubectl get pods -A
+kubectl get pods -n jenkins
 
 
